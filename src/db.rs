e27352b9e31dd424bdb55aaae62c9403cd889732
Ben-PH <benphawke@gmail.com>
2020-08-18 08:31:52 +0200

    Add GET a subjects topics api

--- src/db.rs
+++ src/db.rs
@@ -2,9 +2,12 @@ use actix_web::{get, web, HttpResponse, Responder, Result};
 #[allow(unused_imports)]
 use futures::stream::StreamExt;
 use mongodb::{error::Result as MResult, Collection};
+use shared::{Field, Subject, SubjectId, Topic, TopicId};
+use std::collections::HashMap;
+use std::rc::Rc;
 
 pub fn config(cfg: &mut web::ServiceConfig) {
-    cfg.service(get_subjects);
+    cfg.service(get_subjects).service(get_topics);
 }
 
 pub struct DataBase {
@@ -13,30 +16,73 @@ pub struct DataBase {
     pub learning_objectives: Collection,
 }
 
+#[get("topics/{subj_id}")]
+pub async fn get_topics(
+    db: web::Data<DataBase>,
+    subj_id: web::Path<SubjectId>,
+) -> Result<impl Responder> {
+    match get_topic_list(db, *subj_id).await {
+        Ok(subs) => Ok(HttpResponse::Ok()
+            .json::<HashMap<TopicId, Topic>>(subs)
+            .await),
+        Err(_) => Ok(HttpResponse::InternalServerError()
+            .reason("pull-graph gave error")
+            .await),
+    }
+}
+async fn get_topic_list(
+    db: web::Data<DataBase>,
+    subj_id: SubjectId,
+) -> MResult<HashMap<TopicId, Topic>> {
+    let subj = db
+        .subjects
+        .find_one(bson::doc! {"id": subj_id}, None)
+        .await?
+        .unwrap();
+    let top_objs = subj.get("topics").unwrap();
+    let mut arr = db
+        .topics
+        .find(bson::doc! {"_id" : {"$in" : top_objs}}, None)
+        .await?;
+    let mut v: HashMap<TopicId, Topic> = HashMap::new();
+    while let Some(Ok(topic)) = arr.next().await {
+        let id = topic.get_i32("id").unwrap();
+        let name = topic.get_str("name").unwrap();
+        v.insert(id, Topic::init(id, name.to_string()));
+    }
+    Ok(v)
+}
 #[get("subjects")]
 pub async fn get_subjects(db: web::Data<DataBase>) -> Result<impl Responder> {
     match pull_subject_graph(db).await {
-        Ok(subs) => Ok(HttpResponse::Ok().json::<Vec<shared::Subject>>(subs).await),
+        Ok(subs) => Ok(HttpResponse::Ok()
+            .json::<HashMap<SubjectId, Subject>>(subs)
+            .await),
         Err(_) => Ok(HttpResponse::InternalServerError()
             .reason("pull-graph gave error")
             .await),
     }
 }
 
-async fn pull_subject_graph(db: web::Data<DataBase>) -> MResult<Vec<shared::Subject>> {
+async fn pull_subject_graph(db: web::Data<DataBase>) -> MResult<HashMap<SubjectId, Subject>> {
     let mut cursor = db.subjects.find(None, None).await?;
 
     // TODO: Do this idiomatically.
-    let mut v = Vec::<shared::Subject>::new();
+    let mut v: HashMap<SubjectId, Subject> = HashMap::new();
     while let Some(Ok(doc)) = cursor.next().await {
+        let id = doc.get_i32("id").unwrap();
         let next_name = doc.get_str("name").unwrap().to_string();
-        let next_field = shared::Field::ComputerScience;
-        let next_topic = std::collections::HashSet::<shared::Topic>::new();
-        v.push(shared::Subject {
-            name: next_name,
-            field: next_field,
-            topics: next_topic,
-        });
+        let next_field = Field::ComputerScience;
+        let next_topic = std::collections::HashMap::<TopicId, Rc<Topic>>::new();
+        v.insert(
+            id,
+            Subject {
+                id,
+                name: next_name,
+                field: next_field,
+                topics: next_topic,
+            },
+        );
     }
     Ok(v)
 }
